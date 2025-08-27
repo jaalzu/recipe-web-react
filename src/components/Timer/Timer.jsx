3e7a6c0742c2bca31910b42434932a48ac1db681
Javier <javieralzuu@gmail.com>
2025-08-26 21:05:17 -0300

    timer css done

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -3,7 +3,10 @@ import  styles  from './Timer.module.css'
 export default function Timer(){
     return (
         <>
-            <div className={styles.timerShow}></div>
+            <div className={styles.timerContainer}>
+                <div className={styles.timeShow}>00:00:00</div>
+                <p className={styles.dateShow}>01/01/2024</p>
+            </div>
         </>
     )
 }
