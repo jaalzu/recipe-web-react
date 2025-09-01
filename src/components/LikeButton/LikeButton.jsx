ca4738aed817dfd8b16fd695682394b939aea2d0
Javier <javieralzuu@gmail.com>
2025-09-01 00:58:34 -0300

    like button done

--- src/components/LikeButton/LikeButton.jsx
+++ src/components/LikeButton/LikeButton.jsx
@@ -1,10 +1,23 @@
 import { useState } from 'react'
 import styles from './LikeButton.module.css'
+import Button from '../Button/Button'
+
 
 export default function LikeButton(){
-    const [like,setLike] = useState(false)
+    const [isLiked,setIsLiked] = useState(false)
+
+    const handleClickLike = () => {
+           setIsLiked(!isLiked)
+    }
     
     return(
-        <div className={styles.likeButton}>♥ 0 Likes</div>
+        <div className={styles.likeButton}>
+            <Button
+            onClick={handleClickLike}
+            className={isLiked ? styles.liked : styles.notLiked}
+            >
+            {isLiked ? "♥ 1 Likes" : '♥ 0 Likes'}
+            </Button>
+            </div>
     )
 }
