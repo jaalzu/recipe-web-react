2d16d030b345ec9b6c6cad870a76cf2bcc003949
Javier <javieralzuu@gmail.com>
2025-09-02 04:17:49 -0300

    almost finish star rating

--- src/components/StarRating/StarRating.jsx
+++ src/components/StarRating/StarRating.jsx
@@ -3,11 +3,19 @@ import styles from './StarRating.module.css'
 
 export default function StarRating(){
     const [rating,setRating] = useState(0)
+
+    const stars = [1, 2, 3, 4, 5];
+
+  
     return(
         <div className={styles.StarRating}>
-            <p>
-           ☆ ☆ ☆ ☆ ☆★
-            </p>
+            {stars.map((star) => (
+                <span key ={star} onClick={() => setRating(star)}>
+                    {star <= rating ? "★" : "☆"}
+                </span>
+            ))}
+            
+            <br />
         <span>Has calificado {rating} estrellas</span>
         </div>
     )
