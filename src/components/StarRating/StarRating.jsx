626c1f3424c03fb4d189ed4d408f987a22c0c289
Javier <javieralzuu@gmail.com>
2025-09-02 04:39:22 -0300

    hover add in star rating

--- src/components/StarRating/StarRating.jsx
+++ src/components/StarRating/StarRating.jsx
@@ -3,20 +3,31 @@ import styles from './StarRating.module.css'
 
 export default function StarRating(){
     const [rating,setRating] = useState(0)
+      const [hover, setHover] = useState(0); 
+
 
     const stars = [1, 2, 3, 4, 5];
-
   
     return(
         <div className={styles.StarRating}>
             {stars.map((star) => (
-                <span key ={star} onClick={() => setRating(star)}>
-                    {star <= rating ? "★" : "☆"}
+                <span className={styles.star} 
+                key ={star} 
+                onClick={() => setRating(star)}
+                onMouseEnter={() => setHover(star)}
+                onMouseLeave={() => setHover(0)}
+                style={{
+              color: star <= (hover || rating) ? '#ffd700' : '#ddd'
+            }}
+                >
+                 {star <= (hover || rating) ? "★" : "☆"}
                 </span>
             ))}
-            
+
             <br />
-        <span>Has calificado {rating} estrellas</span>
+        <span>Has calificado {rating}/{stars.length} estrellas </span>
         </div>
     )
 }
+
+
