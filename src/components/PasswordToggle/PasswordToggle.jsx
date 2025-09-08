011d727cb0d3d57649a3ce7f2981fa73d9295e39
Javier <javieralzuu@gmail.com>
2025-09-08 19:40:38 -0300

    trying useeffect

--- src/components/PasswordToggle/PasswordToggle.jsx
+++ src/components/PasswordToggle/PasswordToggle.jsx
@@ -1,13 +1,23 @@
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 import styles from './PasswordToggle.module.css';
 
 export default function PasswordToggle() {
-  return (
-    <div className={styles.passwordContainer}>
-      <div className={styles.passwordInput}>
-        <input type="password" placeholder="Contraseña" />
-        <p>👁️</p>
-      </div>
-    </div>
-  );
+  // return (
+  //   <div className={styles.passwordContainer}>
+  //     <div className={styles.passwordInput}>
+  //       <input type="password" placeholder="Contraseña" />
+  //       <p>👁️</p>
+  //     </div>
+  //   </div>
+  // );
+
+  const [count, setCount] = useState(0);
+
+  // Este effect se ejecuta después de cada render
+  useEffect(() => {
+    document.title = `Clicks: ${count}`;
+    console.log(count)
+  });
+
+  return <button onClick={() => setCount(count + 1)}>Clickeame</button>;
 }
