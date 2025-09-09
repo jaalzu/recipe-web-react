1d023a59a06fdd70df3b2eae90784af2cb91cf13
Javier <javieralzuu@gmail.com>
2025-09-08 22:12:53 -0300

    password toggle done

--- src/components/PasswordToggle/PasswordToggle.jsx
+++ src/components/PasswordToggle/PasswordToggle.jsx
@@ -2,22 +2,29 @@ import {useState,useEffect} from 'react'
 import styles from './PasswordToggle.module.css';
 
 export default function PasswordToggle() {
-  // return (
-  //   <div className={styles.passwordContainer}>
-  //     <div className={styles.passwordInput}>
-  //       <input type="password" placeholder="Contraseña" />
-  //       <p>👁️</p>
-  //     </div>
-  //   </div>
-  // );
+  const [password,setPassword] = useState("")
+  const [showPassword,setShowPassword] = useState(false)
+  
+  const handlePassword = (e) => {
+    setPassword(e.target.value)
+  }
 
-  const [count, setCount] = useState(0);
-
-  // Este effect se ejecuta después de cada render
-  useEffect(() => {
-    document.title = `Clicks: ${count}`;
-    console.log(count)
-  });
-
-  return <button onClick={() => setCount(count + 1)}>Clickeame</button>;
+  const togglePassword = () => {
+    setShowPassword(!showPassword)
+  }
+  return (
+    <div className={styles.passwordContainer}>
+      <div className={styles.passwordInput}>
+        <input 
+        type={showPassword ? 'text' : 'password'} 
+        placeholder="Contraseña" 
+        value={password}
+        onChange={handlePassword}
+        />
+        <p onClick={togglePassword}>
+          {showPassword ? "🙈" : "👁️"}
+          </p>
+      </div>
+    </div>
+  );
 }
