262a205bba6dbaabb847e6bc67dee89c33847c1a
Javier <javieralzuu@gmail.com>
2025-09-09 21:26:38 -0300

    cronómetro done

--- src/components/Cronometro/Cronometro.jsx
+++ src/components/Cronometro/Cronometro.jsx
@@ -2,24 +2,58 @@ import { useState, useEffect } from 'react';
 import styles from './Cronometro.module.css';
 
 export default function Cronometro() {
-   
+   const [time,setTime] = useState(0)
+   const [isRunning,setIsRunning] = useState(false)
+    
+    useEffect(() => {
+        let interval = null;
+
+        if(isRunning){
+            interval = setInterval(() => {
+                setTime(prev => prev + 1);
+            },1000);
+        }else{
+            clearInterval(interval)
+        }
+
+        return () => clearInterval(interval)
+    },[isRunning])
+
+
+    const formatTime = (seconds) => {
+        const h = String(Math.floor(seconds / 3600)).padStart(2,'0');
+        const m = String(Math.floor((seconds % 3600) / 60 )).padStart(2,'0');
+        const s = String(seconds % 60).padStart(2,'0');
+        return `${h}:${m}:${s}`
+    }
+
+
+    const handleRunning = () => {
+        setIsRunning(!isRunning)
+    }
+
+    const handleReset = () => {
+        setTime(0)
+        setIsRunning(false)
+    }
 
     return (
         <div className={styles.cronometroContainer}>
             <h3 className={styles.title}>⏱️ Cronómetro</h3>
             <div className={styles.timeDisplay}>
-                00:00:00
+                {formatTime(time)}
             </div>
             <div className={styles.buttonsContainer}>
                 <button 
                     className={styles.startButton}
+                    onClick={handleRunning}
                 >
-                    Start
-                    {/* <span>{isRunning ? '⏸️' : '▶️'}</span> */}
-                    {/* <span>{isRunning ? 'Pausar' : 'Iniciar'}</span> */}
+                    <span>{isRunning ? '⏸️' : '▶️'}</span>
+                    <span>{isRunning ? 'Pausar' : 'Iniciar'}</span>
                 </button>
                 <button 
                     className={styles.resetButton}
+                    onClick={handleReset}
                 >
                     ⟳
                 </button>
